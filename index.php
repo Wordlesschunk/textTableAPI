e29286b042bad511965cfa911706dc0cd700f411
Dylan Jennings <dylan.jennings@wrenkitchens.com>
2024-08-14 18:40:10 +0100

    Created very basic api to create a text based table from a given json payload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Bramus\Router\Router;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+$router = new Router();
+
+$router->post('/create', function() {
+    $output = new BufferedOutput();
+    $table = new Table($output);
+    
+    $input = json_decode(file_get_contents('php://input'), true);
+    
+    // Ensure all keys are provided
+    if (!isset($input['table']['headers'], $input['table']['rows'])) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Incorrect json format!']);
+        return;
+    }
+    
+    $table->setHeaders($input['table']['headers']);
+    $table->addRows($input['table']['rows']);
+    
+    $table->render();
+    
+    $plainTextTable = $output->fetch();
+    
+    header('Content-Type: text/plain');
+    echo $plainTextTable;
+});
+
+$router->run();
